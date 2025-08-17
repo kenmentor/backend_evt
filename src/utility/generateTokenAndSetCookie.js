1e1935ae4391941fac4c9833c7b23731456c5471
mentor <davidnwachukwum@gmail.com>
2025-08-17 13:51:40 +0100

    fixed the upload again

--- src/utility/generateTokenAndSetCookie.js
+++ src/utility/generateTokenAndSetCookie.js
@@ -1,16 +1,14 @@
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 module.exports = (res, userId) => {
+  jwt_api_key = process.env.JWT_API_KEY;
+  const token = jwt.sign({ userId }, jwt_api_key, { expiresIn: "30d" });
+  res.cookie("token", token, {
+    httpOnly: true,
 
-      jwt_api_key = process.env.JWT_API_KEY
-      const token = jwt.sign({ userId }, jwt_api_key, { expiresIn: "30d" })
-      res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
-            maxAge: 30 * 24 * 60 * 60 * 1000 //30day 
-
-      })
-      return token
-}
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30day
+  });
+  return token;
+};
