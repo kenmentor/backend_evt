2b4b71b6289e0bd02e48cd0b120b33c743529ebe
mentor <davidnwachukwum@gmail.com>
2025-09-03 16:36:24 +0100

    ïmproved the payment stuff

--- src/modules/resource.js
+++ src/modules/resource.js
@@ -4,7 +4,7 @@ const resourceSchema = new mongoose.Schema(
   {
     host: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "user",
       required: true,
     },
     title: { type: String, required: true },
